61f820f0c344ca9e37a59e1d5e42c2a2fa47ba6c
xillr8 <carnone.com@gmail.com>
2025-06-25 04:11:44 -0400

    Final: Confirm setup for styled Amplify deploy

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,7 +1,8 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-export default {
-  plugins: [tailwindcss, autoprefixer],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
+
 
