a73a71fb3c579ae7c5844cd053f009cd0b6cc8d1
xillr8 <carnone.com@gmail.com>
2025-06-29 04:19:58 -0400

    Add Button component

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,13 @@
+// src/components/Button.jsx
+import React from "react";
+
 export default function Button({ children, className, ...props }) {
   return (
-    <button className={`transition duration-200 font-semibold ${className}`} {...props}>
+    <button className={className} {...props}>
       {children}
     </button>
   );
 }
+
 
 
